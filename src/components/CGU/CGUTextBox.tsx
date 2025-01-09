87651c525948c95d80863fc7617060ff70115962
Jeremy Jebane <ti.boug.95100@gmail.com>
2025-01-09 14:05:02 +0100

    gpu good

--- src/components/CGU/CGUTextBox.tsx
+++ src/components/CGU/CGUTextBox.tsx
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
 const CGUTextBox = () => {
-    const [cguContent, setCguContent] = useState('');
+  const [cguContent, setCguContent] = useState("")
 
-    useEffect(() => {
-        fetch("/CGU.txt")  // Attention : Chemin relatif depuis le dossier public
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Le fichier CGU.txt est introuvable.');
-                }
-                return response.text();
-            })
-            .then((text) => setCguContent(text))
-            .catch((error) => console.error('Erreur de chargement :', error));
-    }, []);
+  useEffect(() => {
+    fetch("/CGU.txt") // Attention : Chemin relatif depuis le dossier public
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Le fichier CGU.txt est introuvable.")
+        }
+        return response.text()
+      })
+      .then((text) => setCguContent(text))
+      .catch((error) => console.error("Erreur de chargement :", error))
+  }, [])
 
-    return (
-        <div className="container mx-auto py-10 px-8">
-            <h1 className="text-4xl font-bold mb-6 text-center">Conditions Générales d'Utilisation</h1>
-            <textarea
-                value={cguContent}
-                readOnly
-                className="h-[800px] bg-myprimary w-full border border-gray-300 rounded-lg p-4 text-sm text-justify resize-none " 
-            />
-        </div>
-    );
-};
+  return (
+    <div className="container mx-auto py-10 px-8">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Conditions Générales d'Utilisation
+      </h1>
+      <textarea
+        value={cguContent}
+        readOnly
+        className="h-[800px] bg-myprimary w-full border border-gray-300 rounded-lg p-4 text-sm text-justify resize-none "
+      />
+    </div>
+  )
+}
 
-export default CGUTextBox;
+export default CGUTextBox
--- src/pages/cgu.tsx
+++ src/pages/cgu.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import CGUTextBox from '../components/CGU/CGUTextBox'; // Import direct avec ?raw (Vite spécifique)
+import React, { useState, useEffect } from "react"
+import CGUTextBox from "../components/CGU/CGUTextBox" // Import direct avec ?raw (Vite spécifique)
 
 const CGU = () => {
+  return (
+    <main className=" w-5/6 flex items-center justify-center">
+      <CGUTextBox />
+    </main>
+  )
+}
 
-    return (
-      <main className=" w-5/6 flex items-center justify-center">
-          <CGUTextBox />
-      </main>
-  );
-};
-
-export default CGU;
+export default CGU
