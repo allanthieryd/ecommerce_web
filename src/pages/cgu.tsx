fd0fe8d1e92296a2d17123f918b69988ca1fbd77
Jeremy Jebane <ti.boug.95100@gmail.com>
2025-01-09 14:02:31 +0100

    CGU

--- src/pages/cgu.tsx
+++ src/pages/cgu.tsx
@@ -1,32 +1,13 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import CGUTextBox from '../components/CGU/CGUTextBox'; // Import direct avec ?raw (Vite spécifique)
 
-const CGU: React.FC = () => {
-  return (
-    <>
-      <main>
-        <h1 className="text-4xl font-bold mb-6 text-center">
-          Conditions Générales d’Utilisation (CGU)
-        </h1>
-        <section className="mb-8 ml-4 md:ml-0">
-          <h2 className="text-2xl font-semibold mb-4">1. Introduction</h2>
-          <p className="text-lg leading-relaxed">
-            Bienvenue sur notre site. En utilisant ce site, vous acceptez les
-            conditions générales d’utilisation suivantes...
-          </p>
-        </section>
-        <section className="mb-8 ml-4 md:ml-0">
-          <h2 className="text-2xl font-semibold mb-4">
-            2. Utilisation du Site
-          </h2>
-          <p className="text-lg leading-relaxed">
-            Vous vous engagez à utiliser ce site de manière légale et
-            respectueuse...
-          </p>
-        </section>
-        {/* Ajoutez d’autres sections nécessaires pour vos CGU */}
+const CGU = () => {
+
+    return (
+      <main className=" w-5/6 flex items-center justify-center">
+          <CGUTextBox />
       </main>
-    </>
-  )
-}
+  );
+};
 
-export default CGU
+export default CGU;
